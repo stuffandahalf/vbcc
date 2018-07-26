01bab4b7b0d95fe99dd563545ab210f2581e0426
Gregory Norton <gregory.norton@me.com>
2018-07-25 21:21:29 -0400

    Added initial machine definitions for mc6809

---
+++ machines/mc6809/machine.c
@@ -0,0 +1,14 @@
+#include "supp.h"
+
+char *cg_copyright = "Copyright Gregory Norton 2018";
+
+zmax char_bit;
+zmax align[MAX_TYPE + 1];
+zmax maxalign;
+zmax sizetab[MAX_TYPE + 1];
+zmax t_min[MAX_TYPE + 1];
+zumax t_max[MAX_TYPE + 1];
+zumax tu_max[MAX_TYPE + 1];
+
+char *reg_names[MAXR + 1] = { "A", "B", "X", "Y" };
+zmax regsize[MAXR + 1];/* = { 1, 1, 2, 2 };*/
